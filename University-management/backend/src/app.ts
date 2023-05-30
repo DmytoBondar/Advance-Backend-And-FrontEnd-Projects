df0a63b1676601b1d6777d653811ffc417324805
ujjalz <ujjalz@thegates.biz>
2023-05-30 19:24:41 +0600

    Update

--- University-management/backend/src/app.ts
+++ University-management/backend/src/app.ts
@@ -1,13 +1,12 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 
-const app:Application = express();
-
+    const app:Application = express();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", () => {
     console.log("working well");
 })
 
