f2310c6feb90c4905ebdeaf2621f2dd5b76c32a8
ujjalzaman <muzzwal97@gmail.com>
2023-06-16 19:49:09 +0600

    Implemeted Pagination on academic-semester

--- University-management/backend/src/app/modules/acadamicSemester/acadamicSemester.service.ts
+++ University-management/backend/src/app/modules/acadamicSemester/acadamicSemester.service.ts
@@ -1,39 +1,61 @@
+import { SortOrder } from "mongoose";
 import ApiError from "../../../errors/ApiErrors";
+import { paginationHelper } from "../../../helpers/paginationHelper";
+import { IGenericResponse, IPaginationOptions } from "../../../interfaces/pagination";
 import { AcademicSemesterMapper } from "./acadamicSemester.constant";
 import { IAcademicSemester } from "./acadamicSemester.interface";
 import { AcademicSemester } from "./acadamicSemester.model";
 
-const createSemester = (payload: IAcademicSemester): Promise<IAcademicSemester> => {
+const createSemester = async(payload: IAcademicSemester): Promise<IAcademicSemester> => {
 
     if (AcademicSemesterMapper[payload.title] !== payload.code) {
         throw new ApiError(409, "Could'nt mistach title and code!")
     }
-    const result = AcademicSemester.create(payload);
+    const result =await AcademicSemester.create(payload);
     return result;
 }
 
-const getAllAcademicSemester = (): Promise<IAcademicSemester[]> => {
-    const result = AcademicSemester.find();
-    return result;
+const getAllAcademicSemester = async (payload: IPaginationOptions): Promise<IGenericResponse<IAcademicSemester[]>> => {
+    const {page, limit, skip, sortBy, sortOrder} = paginationHelper.calculatePagination(payload)
+    
+    const sortCondition: {[key: string]: SortOrder} = {};
+
+    if(sortBy && sortOrder){
+        sortCondition[sortBy] = sortOrder;
+    }
+    const result = await AcademicSemester.find().sort(sortCondition).skip(skip).limit(limit);
+    const total = await AcademicSemester.countDocuments();
+    return {
+        meta: {
+          page,
+          limit,
+          total,
+        },
+        data: result,
+      };
 }
-const getByIdAcademicSemester = (id:string): Promise<IAcademicSemester | null> => {
-    const result = AcademicSemester.findById(id);
+
+
+
+
+const getByIdAcademicSemester =async (id:string): Promise<IAcademicSemester | null> => {
+    const result =await AcademicSemester.findById(id);
     return result;
 }
 
-const deleteAcademicSemester = (id:string): Promise<IAcademicSemester | null> => {
-    const result = AcademicSemester.findByIdAndDelete(id);
+const deleteAcademicSemester =async (id:string): Promise<IAcademicSemester | null> => {
+    const result =await AcademicSemester.findByIdAndDelete(id);
     return result;
 }
 
-const updateAcademicSemester =(id: string, payload: Partial<IAcademicSemester>): Promise<IAcademicSemester | null> => {
+const updateAcademicSemester =async(id: string, payload: Partial<IAcademicSemester>): Promise<IAcademicSemester | null> => {
 
     if (
         payload.title && payload.code &&
         AcademicSemesterMapper[payload.title] !== payload.code) {
         throw new ApiError(409, "Could'nt mistach title and code!")
     }
-    const result = AcademicSemester.findOneAndUpdate({_id: id}, payload, {new : true})
+    const result =await AcademicSemester.findOneAndUpdate({_id: id}, payload, {new : true})
     return result;
 }
 export const AcademicService = {
---
+++ University-management/backend/src/helpers/paginationHelper.ts
@@ -0,0 +1,31 @@
+import { SortOrder } from "mongoose"
+
+type IOptions = {
+    limit?: number,
+    page?: number,
+    sortBy?: string,
+    sortOrder?: SortOrder
+}
+type IOptionsReturn = {
+    page: number,
+    limit: number,
+    skip: number,
+    sortBy: string,
+    sortOrder: SortOrder
+}
+const calculatePagination = (options: IOptions): IOptionsReturn => {
+    const page = Number (options.page || 1);
+    const limit = Number(options.limit || 10);
+    const skip = (page -1)*limit;
+
+    const sortBy = options.sortBy || 'createdAt';
+    const sortOrder = options.sortOrder || 'desc';
+    
+    return {
+        page, limit, skip, sortBy, sortOrder
+    }
+}
+
+export const paginationHelper = {
+    calculatePagination
+}
